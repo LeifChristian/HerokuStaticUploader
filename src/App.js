fc524fc479a5999cc2c9a804e5ebb23ef181a839
mtmusicandart@gmail.com <mtmusicandart@gmail.com>
2022-01-27 16:21:06 -0700

    s

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class App extends Component {
+  componentDidMount() {
+    window.location.replace("http://54.193.226.28:3000/");
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default App;
