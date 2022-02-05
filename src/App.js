c0ab265b9067d8088aba22e30700a321190bedcb
lchristian@unitedlocating.com-at-530555730387 <mtmusicandart@gmail.com>
2022-02-05 10:39:34 -0700

    updated IP

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 export class App extends Component {
   componentDidMount() {
-    window.location.replace("http://54.193.226.28:3000/");
+    window.location.replace("http://54.193.124.151:3000//");
   }
 
   render() {
